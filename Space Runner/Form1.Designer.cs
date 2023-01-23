6985b6599fb599040a9f15e0ce9aa5e7716b4fdb
Saahil Patel <saahpate456@ed.amdsb.ca>
2023-01-23 11:29:55 -0500

    Add project files.

---
+++ Space Runner/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Space_Runner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.abilityLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.gameoverLabel = new System.Windows.Forms.Label();
+            this.fuelLabel = new System.Windows.Forms.Label();
+            this.gameLoop = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.timeLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.timeLabel.Location = new System.Drawing.Point(12, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(100, 22);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "timeLabel";
+            // 
+            // abilityLabel
+            // 
+            this.abilityLabel.AutoSize = true;
+            this.abilityLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.abilityLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.abilityLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.abilityLabel.Location = new System.Drawing.Point(12, 438);
+            this.abilityLabel.Name = "abilityLabel";
+            this.abilityLabel.Size = new System.Drawing.Size(130, 22);
+            this.abilityLabel.TabIndex = 2;
+            this.abilityLabel.Text = "abilityLabel";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.titleLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.titleLabel.Location = new System.Drawing.Point(344, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(110, 22);
+            this.titleLabel.TabIndex = 3;
+            this.titleLabel.Text = "titleLabel";
+            // 
+            // gameoverLabel
+            // 
+            this.gameoverLabel.AutoSize = true;
+            this.gameoverLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.gameoverLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameoverLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.gameoverLabel.Location = new System.Drawing.Point(325, 44);
+            this.gameoverLabel.Name = "gameoverLabel";
+            this.gameoverLabel.Size = new System.Drawing.Size(140, 22);
+            this.gameoverLabel.TabIndex = 4;
+            this.gameoverLabel.Text = "gameoverLabel";
+            this.gameoverLabel.Click += new System.EventHandler(this.gameoverLabel_Click);
+            // 
+            // fuelLabel
+            // 
+            this.fuelLabel.AutoSize = true;
+            this.fuelLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.fuelLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fuelLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.fuelLabel.Location = new System.Drawing.Point(12, 486);
+            this.fuelLabel.Name = "fuelLabel";
+            this.fuelLabel.Size = new System.Drawing.Size(100, 22);
+            this.fuelLabel.TabIndex = 5;
+            this.fuelLabel.Text = "fuelLabel";
+            // 
+            // gameLoop
+            // 
+            this.gameLoop.Enabled = true;
+            this.gameLoop.Interval = 20;
+            this.gameLoop.Tick += new System.EventHandler(this.gameLoop_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.fuelLabel);
+            this.Controls.Add(this.gameoverLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.abilityLabel);
+            this.Controls.Add(this.timeLabel);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label abilityLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label gameoverLabel;
+        private System.Windows.Forms.Label fuelLabel;
+        private System.Windows.Forms.Timer gameLoop;
+    }
+}
+
---
+++ Space Runner/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Space_Runner
+{
+    public partial class Form1 : Form
+    {
+        //rocket
+        Rectangle rocket = new Rectangle(200, 250, 20, 20);
+        int rocketspeed = 9;
+        int acceleration = 0;
+
+        //Asteroid
+        List<Rectangle> asteroidsRight = new List<Rectangle>();
+        int asteroidSpeeds = 5;
+        int asteroidSize = 15;
+
+        //random
+        Random randGen = new Random();
+
+        //movement
+        bool wDown = false;
+        bool sDown = false;
+
+        //paint
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+        SolidBrush grayBrush = new SolidBrush(Color.Gray);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+            }
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(whiteBrush, rocket);
+            
+            for (int i = 0; i < asteroidsRight.Count; i++)
+            {
+                e.Graphics.FillRectangle(grayBrush, asteroidsRight[i]);
+            }
+        }
+
+        private void gameLoop_Tick(object sender, EventArgs e)
+        {
+            //move rocket 
+            if (wDown == true && rocket.Y > 0)
+            {
+                rocket.Y -= rocketspeed;
+                acceleration++;
+                rocket.Y -= acceleration;
+            }
+            if (sDown == true && rocket.Y < this.Height - rocket.Height)
+            {
+                rocket.Y += rocketspeed;
+                acceleration--;
+                rocket.Y -= acceleration;
+            }
+            if (wDown == false && sDown == false)
+            {
+                if (acceleration > 0)
+                {
+                    acceleration--;
+                }
+                else if (acceleration < 0)
+                {
+                    acceleration++;
+                }
+            }
+
+            //Random asteroid
+            int randValue = randGen.Next(1, 550);
+            int chance = randGen.Next(1, 500);
+            if (chance < 35)
+            {
+                asteroidsRight.Add(new Rectangle(this.Width, randValue, asteroidSize, asteroidSize));
+            }
+
+            //move asteroid objects from right
+            for (int i = 0; i < asteroidsRight.Count; i++)
+            {
+                int x = asteroidsRight[i].X - asteroidSpeeds;
+                asteroidsRight[i] = new Rectangle(x, asteroidsRight[i].Y, asteroidSize, asteroidSize);
+            }
+
+            Refresh();
+        }
+        private void gameoverLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
